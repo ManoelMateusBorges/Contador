a606dd2d21daea7d95c1ef39c83ca4458543e336
Manoel Mateus <mateusmanoel@lampp-it.com.br>
2022-10-21 01:43:49 -0300

    aplicação finalizada

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,2 +1,19 @@
 "use strict";
-
+let num = 0;
+const btns = document.querySelectorAll(".btn");
+const valor = document.querySelector("#valor");
+btns.forEach((btn) => {
+    btn.addEventListener("click", (bt) => {
+        const style = btn.classList;
+        if (style.contains("btn-secondary")) {
+            num++;
+        }
+        else if (style.contains("btn-primary")) {
+            num--;
+        }
+        else {
+            num = 0;
+        }
+        valor.textContent = num.toString();
+    });
+});
